40fb96449696b1e7457b0e6327919befc768558e
Sebastián Valenzuela Garrido <svalenzuelag@uc.cl>
2024-04-17 21:50:31 -0400

    esquema_canales

---
+++ requests/index.js
@@ -0,0 +1,38 @@
+const mqtt = require("mqtt");
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+dotenv.config();
+
+const HOST = "broker.iic2173.org";
+const PORT = 9000;
+const USER = "students";
+const PASSWORD = "iic2173-2024-1-students";
+const TOPIC = "flights/requests";
+
+const client = mqtt.connect(`mqtt://${HOST}:${PORT}`, {
+    username: USER,
+    password: PASSWORD,
+});
+
+client.on("connect", () => {
+    console.log("Connected to MQTT broker");
+
+    client.subscribe(TOPIC, (err) => {
+        if (err) {
+            console.error("Error subscribing to topic", err);
+        } else {
+            console.log(`Subscribed to topic ${TOPIC}`);
+        }
+    });
+});
+
+client.on("message", (topic, message) => {
+    // Logica para enviar solicitud al broker
+});
+
+async function sendRequestToBroker(request) {
+    // Logica funcion para enviar solicitud al broker
+}
+
+module.exports = client;
---
+++ validations/index.js
@@ -0,0 +1,34 @@
+const mqtt = require("mqtt");
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+dotenv.config();
+
+const HOST = "broker.iic2173.org";
+const PORT = 9000;
+const USER = "students";
+const PASSWORD = "iic2173-2024-1-students";
+const TOPIC = "flights/validation";
+
+const client = mqtt.connect(`mqtt://${HOST}:${PORT}`, {
+    username: USER,
+    password: PASSWORD,
+});
+
+client.on("connect", () => {
+    console.log("Connected to MQTT broker");
+
+    client.subscribe(TOPIC, (err) => {
+        if (err) {
+            console.error("Error subscribing to topic", err);
+        } else {
+            console.log(`Subscribed to topic ${TOPIC}`);
+        }
+    });
+});
+
+client.on("message", (topic, message) => {
+    // Logica para recibir validacion del broker
+});
+
+module.exports = client;
